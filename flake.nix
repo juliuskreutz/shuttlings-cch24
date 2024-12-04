632e0c89d19d4c0e91e3f12c5ac9a658e887f51c
Julius Kreutz <julius@kreutz.dev>
2024-12-04 22:10:34 +0100

    fix: nix flake

--- flake.nix
+++ flake.nix
@@ -17,6 +17,32 @@
         pkgs = import nixpkgs {
           inherit system overlays;
         };
+
+        shuttle = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "cargo-shuttle";
+          version = "0.49.0";
+
+          doCheck = false;
+
+          src = pkgs.fetchCrate {
+            inherit pname version;
+            hash = "sha256-yRIRu0F4BiM/KfKTwty+uzH4uvhbiYd+XxzAQoxyb6o=";
+          };
+
+          cargoHash = "sha256-8F8iUprhSFB3LZBEl5XNObvhc301/akGpP/nI9YFZ+4=";
+        };
+
+        cch24-validator = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "cch24-validator";
+          version = "2.0.1";
+
+          src = pkgs.fetchCrate {
+            inherit pname version;
+            hash = "sha256-AdKFVGvRe3xG3bzaaAJ95NSWjJ4oCgmB8Y7UcaDVOiM=";
+          };
+
+          cargoHash = "sha256-1BaR4Rc62INH9rP4y1D2O15cyT7H1D1amXK4hKC8xmk=";
+        };
       in {
         devShells.default = with pkgs;
           mkShell {
@@ -24,6 +50,9 @@
               rust-bin.stable.latest.default
               rust-analyzer
               taplo
+              cargo-watch
+              shuttle
+              cch24-validator
             ];
           };
       }
